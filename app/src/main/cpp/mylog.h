e11d673fcf1f13d36d4427477f4bdc5f025dc9c0
ZhuMingren <zhumingren@secretlisa.com>
2018-01-11 18:17:51 +0800

    Add rapidjson header file

---
+++ app/src/main/cpp/mylog.h
@@ -0,0 +1,23 @@
+#include <android/log.h>
+
+#ifndef _MYLOG_H_
+#define _MYLOG_H_
+
+
+#define   LOG_TAG    "native_log"
+
+#define DEBUG_SIGN 1 // 1 为校验, 0为不校验
+//#define DEBUG_LOG  // 通过注释该行代码关闭log开关
+
+#ifdef DEBUG_LOG
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#else
+#define LOGE(...) //__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, LOG_TAG)
+#define LOGD(...) //__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, LOG_TAG)
+#define LOGI(...) //__android_log_print(ANDROID_LOG_INFO, LOG_TAG, LOG_TAG)
+#endif
+
+
+#endif
